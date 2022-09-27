a845a3520bb07218b92e7829c82e0cc2120d3c25
Charli Wild <charli.wild@charlis-mbp.lan>
2022-09-27 11:01:19 +0100

    remove ec2 from outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,13 +1,3 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.test_server.id
-}
-
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.test_server.public_ip
-}
-
 output "bucket_name" {
   description = "state bucket name"
   value       = aws_s3_bucket.terraform_state.bucket_domain_name
