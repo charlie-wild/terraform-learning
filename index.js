4ae6d50ca5aef540c702ec373af035b5517d89fb
Charli Wild <charli.wild@Charlis-MacBook-Pro.local>
2022-09-29 15:42:06 +0100

    add imports

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const serverless = require('serverless-http');
-const express = require('express')
-const app = express()
+const bodyParser = require("body-parser");
+const express = require('express');
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.options('*', cors())
+app.options('*', cors());
 
 app.listen(3001, () => {
   console.log("Server started on port 3001");
 });
  
 app.get('/hello', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
  
 module.exports.handler = serverless(app);
