1d860e7658bee1fd91b3179eb7447c12ca4a3521
Charli Wild <charli.wild@Charlis-MacBook-Pro.local>
2022-09-29 14:41:07 +0100

    updated routing

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.options('*', cors())
+
+app.listen(3001, () => {
+  console.log("Server started on port 3001");
+});
  
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
  
